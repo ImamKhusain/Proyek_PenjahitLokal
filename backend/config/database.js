15a521e0c5467946edab01817deaf1065c7fe30f
026_Adi <adisetyanurpradipta@gmail.com>
2026-05-24 20:20:08 +0700

    begoo

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,32 +1,17 @@
-const { Sequelize } =
-  require("sequelize");
+const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize =
-  new Sequelize(
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
-    process.env.DB_NAME,
-
-    process.env.DB_USER,
-
-    process.env.DB_PASS,
-
-    {
-
-      host:
-        process.env.DB_HOST,
-
-      port:
-        process.env.DB_PORT,
-
-      dialect: "mysql",
-
-      logging: false,
-
-    }
-
-  );
-
-module.exports =
-  sequelize;
+module.exports = sequelize;
